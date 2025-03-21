53dd8b619e8d8b2c3a680d6160c34cf779cd21cb
여현승 <yhs10179@gmail.com>
2025-03-21 14:53:35 +0900

    feat: Frontend CSS 기초 설정
    - 로고 및 폰트 파일 추가
    - 색상(primary color, secondary color) 변수화
    
    S12P21A501-69

---
+++ frontend/dorolaw/src/app/App.tsx
@@ -0,0 +1,13 @@
+import Header from '../widgets/Header';
+import NavBar from '../widgets/NavBar';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
