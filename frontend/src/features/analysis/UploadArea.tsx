46f76a44851c0c5f8a2f4c0ce52cdaa517125b1e
joonsun <rjs7289@gmail.com>
2025-03-25 00:08:13 +0900

    사고영상 업로드 기본 UI 구성 완료

--- frontend/src/features/analysis/UploadArea.tsx
+++ frontend/src/features/analysis/UploadArea.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-
 const UploadArea = () => {
   return (
-    <div className="flex justify-center items-center w-[996px] h-[683px] mx-auto">
-      <div className="w-full h-[600px] border-2 border-dashed rounded-lg flex flex-col justify-center items-center p-6 text-center">
+    <div className="mx-auto flex h-[480px] w-[800px]  items-center justify-center">
+      <div className="flex h-[420px] w-full flex-col items-center justify-center rounded-lg border-2 border-dashed p-6 text-center">
         <p className="text-lg font-medium text-gray-700">
           파일을 이곳에 드롭하세요.
         </p>
--- frontend/src/pages/analysis/VideoUpload.tsx
+++ frontend/src/pages/analysis/VideoUpload.tsx
@@ -2,25 +2,49 @@ import UploadArea from '@/features/analysis/UploadArea';
 
 const VideoUpload = () => {
   return (
-    <form className="mt-[76px]">
+    <main className="mt-[50px]">
       {/* 업로드 타이틀 */}
-      <div className="flex items-center justify-between w-[996px] mx-auto px-0">
+      <div className="mx-auto flex w-[800px] items-center justify-between px-0">
         <div>
           <div className="flex items-center gap-5">
-            <h2 className="text-h2 text-p5">사고 영상 업로드</h2>
+            <h2 className="text-h2 font-bold text-p5">사고 영상 업로드</h2>
             <p className="text-caption text-red-500">* 필수</p>
           </div>
           <p className="text-g3">
             사진/영상 업로드 시 AI가 과실 비율을 분석합니다.
           </p>
         </div>
-        <button className="rounded-[10px] bg-p5 text-g1 px-6 py-2">
+        <button className="rounded-[10px] bg-p5 px-6 py-2 text-g1">
           파일 업로드
         </button>
       </div>
       {/* 영상 업로드 드래그 */}
       <UploadArea />
-    </form>
+      {/* 옵션 */}
+      <div className="mx-auto flex w-[800px] items-center justify-between">
+        <div className="flex items-center gap-5">
+          <h2 className="text-body font-bold text-p5">공개 여부</h2>
+          <input
+            type="checkbox"
+            className="size-4 bg-p5"
+            style={{ accentColor: '#374151' }}
+          />
+          <p className="text-caption text-red-500">* 필수</p>
+        </div>
+        <div className="flex items-center gap-5">
+          <h2 className="text-body font-bold text-p5">개인정보 제공 동의</h2>
+          <input
+            type="checkbox"
+            className="size-4 bg-p5"
+            style={{ accentColor: '#374151' }}
+          />
+          <p className="text-caption text-red-500">* 필수</p>
+        </div>
+      </div>
+      <button className="mx-auto my-9 block rounded-[10px] bg-p5 px-6 py-2 text-g1">
+        분석 요청하기
+      </button>
+    </main>
   );
 };
 
