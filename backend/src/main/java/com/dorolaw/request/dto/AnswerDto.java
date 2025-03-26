ea08ffbe6ffdd2ce4fc9e8d3d5fe6db48ea27c9d
dragonsoup1@gmail.com <dragonsoup1@gmail.com>
2025-03-26 16:55:44 +0900

    feat: 변호사 답변
    - 등록
    - 삭제
    - 수정
    
    S12P21A501-67

---
+++ backend/src/main/java/com/dorolaw/request/dto/AnswerDto.java
@@ -0,0 +1,29 @@
+package com.dorolaw.request.dto;
+
+import com.dorolaw.request.entity.Answer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerDto {
+    private Long answerId;
+    private Long lawyerId;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public static AnswerDto fromEntity(Answer answer) {
+        AnswerDto dto = new AnswerDto();
+        dto.setAnswerId(answer.getAnswerId());
+        dto.setLawyerId(answer.getLawyerId());
+        dto.setContent(answer.getContent());
+        dto.setCreatedAt(answer.getCreatedAt());
+        return dto;
+    }
+}
+
---
+++ backend/src/main/java/com/dorolaw/request/dto/AnswerUpdateDto.java
@@ -0,0 +1,11 @@
+package com.dorolaw.request.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerUpdateDto {
+    private String content;
+}
+
