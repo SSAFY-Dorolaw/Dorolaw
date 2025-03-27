1a67622e71385ac000a2c72fa8dc07a86cf55f6a
joonsun <rjs7289@gmail.com>
2025-03-28 07:03:51 +0900

    feat: 상담유형 선택 토글
    - 전화 상담, 화상 상담, 방문 상담
    - 클릭 시 반응 구현 필요
    
    S12P21A501-98

--- frontend/src/features/reservation/BookingPlan.tsx
+++ frontend/src/features/reservation/BookingPlan.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Phone,
   CalendarDays,
@@ -11,6 +12,12 @@ interface BookingPlanProps {
 }
 
 const BookingPlan = ({ onClose }: BookingPlanProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className="rounded-b-[10px] bg-white p-4">
@@ -21,15 +28,35 @@ const BookingPlan = ({ onClose }: BookingPlanProps) => {
           X
         </p>
         {/* 전화상담 선택 */}
-        <div className="flex cursor-pointer justify-between">
+        <div
+          className="flex cursor-pointer justify-between"
+          onClick={toggleOpen}
+        >
           <div className="my-5 flex items-center">
             <Phone className="size-6" />
             <p className="ml-5 text-h3 font-bold text-p5">상담 종류 선택</p>
           </div>
           <div className="flex items-center">
-            <ChevronUp className="mr-2 mt-1 size-6" />
+            {isOpen ? (
+              <ChevronUp className="mr-2 mt-1 size-6" />
+            ) : (
+              <ChevronDown className="mr-2 mt-1 size-6" />
+            )}
           </div>
         </div>
+        {isOpen && (
+          <div className="mb-3 ml-3 mr-5 flex justify-between">
+            <button className="rounded-lg bg-g3 px-3 py-1.5 text-white">
+              15분 전화상담
+            </button>
+            <button className="rounded-lg bg-g3 px-3 py-1.5 text-white">
+              20분 화상상담
+            </button>
+            <button className="rounded-lg bg-g3 px-3 py-1.5 text-white">
+              30분 방문상담
+            </button>
+          </div>
+        )}
 
         {/* 날짜 선택 */}
         <div className="flex cursor-pointer justify-between">
