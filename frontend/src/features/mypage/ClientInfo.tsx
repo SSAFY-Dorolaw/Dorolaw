37e4942055e616020baffd9b46dab7883e2bcae8
여현승 <yhs10179@gmail.com>
2025-03-28 16:14:17 +0900

    feat: 일반 사용자 마이페이지 UI 추가
    - 프로필사진, 이름, 닉네임, 전화번호, 이메일 표시
    - 전화번호 수정 가능
    
    S12P21A501-112

---
+++ frontend/src/features/mypage/ClientInfo.tsx
@@ -0,0 +1,41 @@
+import { Mail, Smartphone } from 'lucide-react';
+
+interface ClientInfoProps {
+  clientId: number;
+  name: string;
+  phoneNumber: string;
+  email: string;
+}
+
+function ClientInfo({ clientId, name, phoneNumber, email }: ClientInfoProps) {
+  return (
+    <div className="border-gray mx-auto mb-8 max-w-[996px] items-center rounded-[10px] border p-10 text-xl">
+      <div className="flex items-center">
+        <div className="mx-10 size-[100px] rounded-full bg-black"></div>
+        <div>
+          <h3>{name} 님 </h3>
+          <h4>의뢰인_{clientId}</h4>
+        </div>
+      </div>
+      <hr className="mt-10" />
+      <div>
+        <div className="mt-10 flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <Smartphone />
+            <p>{phoneNumber}</p>
+          </div>
+          <button className="typo-button-small rounded-[10px] bg-p2 p-1 px-4">
+            수정
+          </button>
+        </div>
+        <hr className="mt-10" />
+        <div className="mt-10 flex items-center gap-4">
+          <Mail />
+          <p>{email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClientInfo;
