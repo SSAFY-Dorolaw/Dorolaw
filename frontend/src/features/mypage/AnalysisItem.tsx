96fe773fc00ee34728b711cfccc45b40e56dbbd2
hyvnsevng <yh189487@gmail.com>
2025-04-06 03:59:21 +0900

    feat: 과실비율 분석 내역 페이지 UI 수정
    - api 연동 실패 -> 더미데이터 넣어서 UI 수정
    
    S12P21A501-133

--- frontend/src/features/mypage/AnalysisItem.tsx
+++ frontend/src/features/mypage/AnalysisItem.tsx
@@ -1,4 +1,6 @@
+import { FaBalanceScale } from 'react-icons/fa';
 import { Analysis } from './client/model/types';
+import { Globe, LockKeyhole } from 'lucide-react';
 
 interface AnalysisItemProps extends Analysis {
   onClick?: () => void;
@@ -23,17 +25,31 @@ function AnalysisItem({
         <div className="mb-2 w-full gap-4 overflow-hidden px-2 text-left">
           <p className="truncate font-semibold">{fileName}</p>
           <div className="flex gap-1">
+            <div className="t flex items-center gap-1">
+              <FaBalanceScale />
+              <p>AI 과실비율 •</p>
+              {faultRatioA}:{faultRatioB}
+            </div>
+          </div>
+          <div className="flex gap-1 text-p4">
             <p>
               {new Date(reportCreatedAt).getFullYear()}년{' '}
               {new Date(reportCreatedAt).getMonth()}월{' '}
               {new Date(reportCreatedAt).getDate()}일
             </p>
-            <p>|</p>
-            <p>
-              {faultRatioA}:{faultRatioB}
-            </p>
+            <p>•</p>
+            {isPublic ? (
+              <div className="flex items-center gap-1">
+                <Globe size={16} />
+                <span>공개</span>
+              </div>
+            ) : (
+              <div className="flex items-center gap-1 font-bold text-p4">
+                <LockKeyhole size={16} />
+                <span>비공개</span>
+              </div>
+            )}
           </div>
-          <p>사고{isPublic}</p>
         </div>
       </section>
     </div>
--- frontend/src/features/mypage/AnalysisList.tsx
+++ frontend/src/features/mypage/AnalysisList.tsx
@@ -5,12 +5,39 @@ import AnalysisItem from './AnalysisItem';
 const AnalysisList = () => {
   const navigate = useNavigate();
 
-  const { data, isPending, isError, error } = useMyAnalyses();
+  //   const { data, isPending, isError, error } = useMyAnalyses();
 
-  console.log(data);
-  if (isPending) return <h2>로딩 중...</h2>;
-  if (isError)
-    return <h2>에러가 발생했습니다: {error?.message || '알 수 없는 에러'}</h2>;
+  //   console.log(data);
+  //   if (isPending) return <h2>로딩 중...</h2>;
+  //   if (isError)
+  //     return <h2>에러가 발생했습니다: {error?.message || '알 수 없는 에러'}</h2>;
+
+  const data = [
+    {
+      reportId: 3,
+      fileName: 'video003.mp4',
+      faultRatioA: 40,
+      faultRatioB: 60,
+      reportCreatedAt: '2025-04-03T10:30:00',
+      isPublic: true,
+    },
+    {
+      reportId: 2,
+      fileName: 'video002.mp4',
+      faultRatioA: 80,
+      faultRatioB: 20,
+      reportCreatedAt: '2025-04-02T15:00:00',
+      isPublic: false,
+    },
+    {
+      reportId: 1,
+      fileName: 'video001.mp4',
+      faultRatioA: 70,
+      faultRatioB: 30,
+      reportCreatedAt: '2025-04-01T11:00:00',
+      isPublic: true,
+    },
+  ];
 
   return (
     <div className="mb-10 grid w-screen max-w-[1200px] grid-cols-4 justify-between gap-4 text-center">
