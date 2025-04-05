88bd16fb50d2bf28c707cf8ca2c5c2634ad04615
hyvnsevng <yh189487@gmail.com>
2025-04-06 02:23:37 +0900

    fix: ai 과실비율 분석 타입 수정
    - 빌드테스트 완료
    - AnalysisItem 컴포넌트 props 타입 수정
    
    S12P21A501-133

--- frontend/src/features/mypage/AnalysisItem.tsx
+++ frontend/src/features/mypage/AnalysisItem.tsx
@@ -6,32 +6,34 @@ interface AnalysisItemProps extends Analysis {
 
 function AnalysisItem({
   reportId,
-  title,
-  thumbnailImageUrl,
-  accidentalNegligenceRateA,
-  accidentalNegligenceRateB,
-  accidentPlaceType,
+  fileName,
+  faultRatioA,
+  faultRatioB,
+  reportCreatedAt,
   isPublic,
-  createdAt,
+  onClick,
 }: AnalysisItemProps) {
   return (
-    <div className="cursor-pointer rounded-[6px] hover:bg-gray-200">
+    <div
+      className="cursor-pointer rounded-[6px] hover:bg-gray-200"
+      onClick={onClick}
+    >
       <div className="mb-2 aspect-video bg-black"></div>
       <section>
         <div className="mb-2 w-full gap-4 overflow-hidden px-2 text-left">
-          <p className="truncate font-semibold">{title}</p>
+          <p className="truncate font-semibold">{fileName}</p>
           <div className="flex gap-1">
             <p>
-              {new Date(createdAt).getFullYear()}년{' '}
-              {new Date(createdAt).getMonth()}월 {new Date(createdAt).getDate()}
-              일
+              {new Date(reportCreatedAt).getFullYear()}년{' '}
+              {new Date(reportCreatedAt).getMonth()}월{' '}
+              {new Date(reportCreatedAt).getDate()}일
             </p>
             <p>|</p>
             <p>
-              {accidentalNegligenceRateA}:{accidentalNegligenceRateB}
+              {faultRatioA}:{faultRatioB}
             </p>
           </div>
-          <p>사고{accidentPlaceType}</p>
+          <p>사고{isPublic}</p>
         </div>
       </section>
     </div>
