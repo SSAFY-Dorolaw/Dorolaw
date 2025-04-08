b5eea9113f176fe5d8739d878225544eca80eb9f
Doyeong Jung <sonatine789@naver.com>
2025-04-08 16:47:31 +0900

    feat: Add ai/aimodel/mmaction2 source code
    - Add ai/aimodel/mmaction2 source code
    
    S12P21A501-179

---
+++ ai/ai-model/mmaction2/configs/_base_/schedules/adam_30e.py
@@ -0,0 +1,19 @@
+# adam_30e.py
+optim_wrapper = dict(
+    optimizer=dict(type='Adam', lr=1e-3, weight_decay=1e-4),
+    clip_grad=dict(max_norm=40, norm_type=2)
+)
+
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=30,  # ✅ 수정!
+        by_epoch=True,
+        milestones=[20, 40],
+        gamma=0.1)
+]
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=50, val_begin=1, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
