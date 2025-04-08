2bf19776c79e66ff3e5dbaab352118c88b0289bd
여현승 <yhs10179@gmail.com>
2025-04-08 21:45:47 +0900

    feat: 의뢰 답변 작성
    - role이 변호사일 시 답변 작성 버튼 추가
    - role이 GENERAL이고 내가 작성한 의뢰글일 시 변호사 프사 옆에 상담신청
      버튼 추가
    - 상담신청 버튼 클릭 시 변호사 프로필로 넘어가 상담 예약 가능
    
    S12P21A501-193

--- frontend/src/features/consultation/Answer.tsx
+++ frontend/src/features/consultation/Answer.tsx
@@ -1,12 +1,17 @@
 import { FaUserCircle } from 'react-icons/fa';
 import { Answers } from './model/types';
 import { formatDate } from '@/shared/lib/utils/dateFormatter';
+import { useNavigate } from 'react-router-dom';
 
 interface AnswerProps {
   answer: Answers;
+  requestId: number;
+  isMyRequest: boolean;
+  isMyAnswer: boolean;
 }
 
-function Answer({ answer }: AnswerProps) {
+function Answer({ answer, requestId, isMyRequest, isMyAnswer }: AnswerProps) {
+  const navigate = useNavigate();
   return (
     <div className="w-full">
       <hr />
@@ -15,40 +20,26 @@ function Answer({ answer }: AnswerProps) {
           <FaUserCircle size={32} />
         </div>
         <div className="typo-body mx-2">변호사 #{answer.lawyerId}</div>
+        <div className="reservation ml-2">
+          {isMyRequest ? (
+            <button
+              className="rounded-[10px] bg-p5 px-2 text-bodysmall text-p1"
+              onClick={() =>
+                void navigate(`/lawyer/profile/${answer.lawyerId}/${requestId}`)
+              }
+            >
+              상담 예약
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
         <div className="ml-auto text-xs text-gray-500">
           {formatDate(answer.createdAt)}
         </div>
-        <div className="reservation ml-2">
-          <button className="rounded-[10px] bg-p5 px-2 text-bodysmall text-p1">
-            상담 예약
-          </button>
-        </div>
       </header>
       <article className="whitespace-pre-wrap p-4">
-        {answer.content}
-        <p>
-          고민이 많이 되실 거 같습니다. 상담과 자료 검토가 필요한
-          상황입니다.의뢰인님의 문제 상황을 보았을 때 적극적인 법률상담을 추천
-          드립니다. 해결 방향 빠르고 확실한 해결방향을 제공하겠습니다.전화 혹은
-          방문 상담을 진행하시는 걸 추천드립니다. 부담 없이 전화주셔도 됩니다.
-          <br />
-          <br />
-          제공 서비스
-          <br />
-          Step1. 무료 전화 상담을 진행합니다.
-          <br />
-          Step2. 상담 진행 후 소송 진행 여부 등을 협의합니다.
-          <br />
-          Step3. 의뢰인 맞춤형으로 소송 등을 진행합니다.
-          <br />
-          김승소 변호사 대성 국제 법률 사무소
-          <br />
-          01012345678 유사 사건 경험 변호사 경력 30년으로 풍부한 노하우를
-          제공합니다. 그동안 쉴틈없이 매주 법원을 2회 이상 들려 소송 진행 등을
-          왔습니다. 형사 민사 가릴 거 없이 거의 다루어보았고, 관련 자문을
-          진행하였습니다. 또한 의뢰인님의 경우와 같은 유사 사건 경험도 상당수
-          처리하였습니다.
-        </p>
+        <p>{answer.content}</p>
       </article>
     </div>
   );
