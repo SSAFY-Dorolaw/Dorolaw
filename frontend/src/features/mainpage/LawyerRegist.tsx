ec54ba3bd0c62ed68d5fb20aa147d13ed135c964
joonsun <rjs7289@gmail.com>
2025-04-09 02:03:29 +0900

    feat: 변호사 등록 부분 태그 수정
    - div 태그를 section 태그로 수정
    
    S12P21A501-177

--- frontend/src/features/mainpage/LawyerRegist.tsx
+++ frontend/src/features/mainpage/LawyerRegist.tsx
@@ -3,35 +3,32 @@ const LawyerRegist = () => {
     <>
       <section className="mx-[120px] flex justify-between">
         <article className="flex items-center">
+          <div className="flex">
+            <h2 className="my-0 text-h1 font-bold">
+              변호사로 참여하고 싶으신가요?
+            </h2>
+          </div>
+          <br />
           <div>
-            <div className="flex">
-              <h2 className="my-0 text-h1 font-bold">
-                변호사로 참여하고 싶으신가요?
-              </h2>
-            </div>
-            <br />
-            <div>
-              <p className="text-bodymedium font-bold">
-                교통사고 전문 변호사로 등록하고, 의뢰인을 직접 만나보세요.{' '}
-                <br />
-                <br />
-                AI 분석 리포트를 바탕으로 상담을 요청받고 <br />
-                고객과 연결될 수 있습니다. <br />
-                <br />
-                상담 예약, 답변 관리, 일정 관리까지 한 번에!
-              </p>
-            </div>
+            <p className="text-bodymedium font-bold">
+              교통사고 전문 변호사로 등록하고, 의뢰인을 직접 만나보세요. <br />
+              <br />
+              AI 분석 리포트를 바탕으로 상담을 요청받고 <br />
+              고객과 연결될 수 있습니다. <br />
+              <br />
+              상담 예약, 답변 관리, 일정 관리까지 한 번에!
+            </p>
           </div>
         </article>
         <article>
           <img src="/business_man.png" alt="비즈니스맨" />
         </article>
       </section>
-      <div className="my-10 flex items-center justify-center">
+      <section className="my-10 flex items-center justify-center">
         <button className="w-[200px] rounded-[10px] bg-y5 p-3 text-body font-bold text-black transition duration-300 hover:bg-y3">
           변호사 등록하기
         </button>
-      </div>
+      </section>
     </>
   );
 };
